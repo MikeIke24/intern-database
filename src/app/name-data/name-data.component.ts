8fa8386dc98f58add758b3c0016a7464325f9957
MikeIke24 <mnelms48@gmail.com>
2017-07-08 10:51:25 -0500

    Working Click Filter

--- src/app/name-data/name-data.component.ts
+++ src/app/name-data/name-data.component.ts
@@ -16,6 +16,7 @@ import {
 export class NameDataComponent implements OnInit {
   people = [];
   stateExpression: string;
+  visibleId = "";
   constructor(private http: Http) {
     this.http.get('../../assets/data/interndata.json')
       .subscribe(res => res.json().forEach(element => {
@@ -26,6 +27,19 @@ export class NameDataComponent implements OnInit {
 
   ngOnInit(): void {
     }
+  
+  nameClick(event):void{
+    let x = event.path;
+    console.log(this.idExtract(x));
+    this.visibleId = this.idExtract(x)
+  }
 
+  idExtract(location:object){
+    let id = (location[0].id);
+    if(!id){
+      id = (location[1].id);
+    }
+    return id.slice(1);
+  }
   }
 
