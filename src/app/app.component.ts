8f56024d98c2a69814adcf172439082226cd1540
MikeIke24 <mnelms48@gmail.com>
2017-07-17 12:17:41 -0500

    Sidebar logic almost perfect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input
+} from '@angular/core';
+import {
+  Http
+} from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  people = [];
+  stateExpression: string;
+  visibleId = "";
+  constructor(private http: Http) {
+    this.http.get('../../assets/data/interndata.json')
+      .subscribe(res => res.json().forEach(element => {
+        this.people.push(element);
+      }));
+
+  }
+
+  ngOnInit(): void {
+    }
+  
+   
+    
+  nameClick(event):void{
+    let x = event.path;
+    console.log(this.idExtract(x));
+    this.visibleId = this.idExtract(x)
+  }
+
+  idExtract(location:object){
+    let id = (location[0].id);
+    if(!id){
+      id = (location[1].id);
+    }
+    return id.slice(1);
+  }
 }
